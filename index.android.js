75001f2678835a800836aeaeed6b474ba99f145f
IjzerenHein <hrutjes@gmail.com>
2016-04-04 15:50:34 +0200

    Fixed LayoutAnimation not working on Android

--- index.android.js
+++ index.android.js
@@ -1,7 +1,8 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, UIManager} from 'react-native';
 import AppView from './src/AppView';
 
 //Press Cmd+R to reload,{'\n'}
 //Cmd+D or shake for dev menu
 
+UIManager.setLayoutAnimationEnabledExperimental(true);
 AppRegistry.registerComponent('LoeffenNinetiesQuiz', () => AppView);
